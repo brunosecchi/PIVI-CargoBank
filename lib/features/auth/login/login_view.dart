1f2a707ec886f62a4cc9a6524b3c00cbb999af1b
Fernando Lise <fernandolise23@gmail.com>
2022-11-01 01:41:51 -0300

    padding

---
+++ lib/features/auth/login/login_view.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  const LoginView({Key? key}) : super(key: key);
+
+  @override
+  State<LoginView> createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
+  final TextEditingController _credentialController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: _appBar(size),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Form(
+            child: Padding(
+              padding: EdgeInsets.all(75.5),
+            key: _formkey,
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: _credentialController,
+                  decoration: const InputDecoration(
+                    
+                    label: Text('CPF ou CNPJ')
+                    ),
+                ),
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(label: Text('Senha')),
+                ),
+
+                SizedBox(
+                  width: 250,
+                  child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: const Color(0xFFF2796B)),
+                      onPressed: () {
+                        Navigator.pushNamed(context, '/list');
+                      },
+                      child: const Text('Entrar')),
+                ),
+                ),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: const <Widget>[
+                    SizedBox(
+                        width: 70,
+                        child: Divider(
+                          height: 5,
+                          color: Color(0xFFA39797),
+                          thickness: 2,
+                        )),
+                    Text('Ou'),
+                    SizedBox(
+                        width: 70,
+                        child: Divider(
+                          height: 100,
+                          color: Color(0xFFA39797),
+                          thickness: 2,
+                        )),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text('Não é cliente?'),
+                    TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/sign-in');
+                        },
+                        child: const Text(
+                          'Crie sua conta',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, color: Colors.black),
+                        ))
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+        ),
+      ),
+    );
+  }
+
+  AppBar _appBar(Size size) {
+    return AppBar(
+      title: Image.asset(
+        'assets/images/logo.png',
+      ),
+      toolbarHeight: size.height * .3,
+      backgroundColor: const Color(0xFFF2796B),
+    );
+  }
+}
---
+++ lib/routes/route_generator.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:pii6/features/auth/sign_in/sign_in_view.dart';
+import 'package:pii6/features/splash/splash_view.dart';
+import '../features/auth/login/login_view.dart';
+import '../features/new_document/list/list.dart';
+
+class RouteGenerator {
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+    final Object? args = settings.arguments;
+    switch (settings.name) {
+      case '/':
+        {
+          return doNavigation(
+            page: const SplashView(),
+          );
+        }
+      case '/login':
+        {
+          return doNavigation(
+            page: const LoginView(),
+          );
+        }
+      case '/sign-in':
+        {
+          return doNavigation(
+            page: const SignInView(),
+          );
+        }
+      case '/list':
+        {
+          return doNavigation(
+            page: List(),
+          );
+        }
+      default:
+        {
+          return doNavigation(
+            page: const SplashView(),
+          );
+        }
+    }
+  }
+
+  static PageRouteBuilder<
+      Widget Function(
+    BuildContext,
+    Animation<double>,
+    Animation<double>,
+  )> doNavigation({required Widget page}) {
+    return PageRouteBuilder<RoutePageBuilder>(
+      pageBuilder:
+          (BuildContext c, Animation<double> a1, Animation<double> a2) => page,
+      transitionsBuilder: (BuildContext c, Animation<double> anim,
+              Animation<double> a2, Widget child) =>
+          FadeTransition(
+        opacity: anim,
+        child: child,
+      ),
+      transitionDuration: const Duration(milliseconds: 1000),
+    );
+  }
+}
